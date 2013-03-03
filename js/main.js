5737e3d1392c82c17551c3f3c8537f121e040b0f
Wololo <dota@hotmail.ca>
2013-03-03 18:52:36 -0500

    Autocomplete

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	var input = document.getElementById('searchTextField');
+	var options = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'ca'}
+	};
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+
+	$('#searchForm').submit(function(e){
+		var query = $('#searchTextField').val();
+		console.log(query);
+	});
+});
